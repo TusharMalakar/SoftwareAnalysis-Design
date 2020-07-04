cb7a262a236e51d9a15038cb1dafbd0a02b2dc87
TusharMalakar <35859780+TusharMalakar@users.noreply.github.com>
2020-07-04 14:18:51 -0400

    Update JavaBST

--- AVL-BST/BST.java
+++ AVL-BST/BST.java
@@ -1,13 +1,16 @@
+import java.util.*;
+
 public class BST {
     
     // inner node class
-    class Node{
+    class Node
+    {
         // member variables of Node class
-        int data;
-        int left;
-        int right;
+    	int data;
+    	Node left;
+    	Node right;
         
-        // constructor
+        // constructor: assign data and make left and right pointers null 
         Node(int data)
         {
             this.data = data;
@@ -18,13 +21,15 @@ public class BST {
     
     
     // creating an reference of Node as root
-    Node node;
-    
-    // constructor
+    Node root;
+    // constructor : create a empty node in BST
     BST()
     {
-        this.node = null;
+        this.root = null;
     }
+    
+    // store path of bst-value
+	Vector<Integer> pathVector = new Vector<Integer>();
     
     // insert function to insert in BST
     public void insert(int data)
@@ -41,22 +46,22 @@ public class BST {
         else
         {
             //insert small value to the left of this.root.date, if there is data in root
-            if( data < this.root.date )
+            if( data < this.root.data )
             {
                // if this.root left is not null, compare the value of this.root.left is greater or smaller than data
                //   1. if this.root.left is smaller, create new node to the right of this.root.left
                //   2. if this.root.left is greater, create new node to the left of this.root.left
                if(this.root.left != null)
                {
-                   // assign to ths left of this.root.left if data smaller than this.root.left
+                   // assign to the left of this.root.left if data smaller than this.root.left
                    // else, assign to the right of this.root.left
-                   if(data < this.root.left)
+                   if(data < this.root.left.data)
                    {
-                       this.root.left.left = _node;
+                	   this.root.left = _node;
                    }
                    else
                    {
-                        this.root.left.right = _node;
+                	   this.root.left = _node;
                    }
                    
                }
@@ -74,9 +79,9 @@ public class BST {
                //   2. if this.root.right is greater, create new node to the left of this.root.right
                if(this.root.right != null)
                {
-                   // assign to ths left of this.root.right if data smaller than this.root.right
+                   // assign to the left of this.root.right if data smaller than this.root.right
                    // else, assign to the right of this.root.right
-                   if(data < this.root.right)
+                   if(data < this.root.right.data)
                    {
                        this.root.right.left = _node;
                    }
@@ -95,6 +100,45 @@ public class BST {
         }
     }
     
+    
+    public Node search(Node root, int data) 
+    { 
+        // Base Cases: root is null or key is present at root 
+        if (root==null || root.data==data)
+        {
+        	return root; 
+        }
+        else
+        {
+        	// add path to pathVector
+        	pathVector.add(root.data);
+        	// data is greater than root's data 
+            if (root.data > data) 
+            {	
+                return search(root.left, data);
+            }
+            else
+            {
+            	// data is less than root's data 
+                return search(root.right, data); 
+            }  	  
+        }  
+    }
+    
+    public String printPath(Vector<Integer> path)
+    {
+    	String valPath = "";
+    	if( !path.isEmpty() )
+    	{
+    		for(int i=0; i<path.size(); i++)
+    		{
+    			valPath += path.get(i).toString();
+    			valPath += " -> ";
+    		}
+    	}
+    	return valPath;
+    }
+    
     public static void main(String args[]) {
         
         // creating instance of BST
@@ -102,7 +146,12 @@ public class BST {
         bst.insert(5);
         bst.insert(4);
         bst.insert(6);
-      
+        bst.insert(8);
+       
+        //search and print path of 8 
+        int searchVal = 8;
+        bst.search(bst.root, searchVal); 
+        String path = bst.printPath(bst.pathVector);
+        System.out.println(path+searchVal);
     }
 }
-
